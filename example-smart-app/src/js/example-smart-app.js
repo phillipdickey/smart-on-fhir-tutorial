20db393998b31073ed466cba1161e15effa670df
Phillip Dickey <pdickey93@gmail.com>
2018-03-29 12:28:58 -0500

    set conditions on patient

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -29,8 +29,7 @@
         $.when(pt, obv).fail(onError);
         $.when(pt, condition).fail(onError);
 
-        $.when(pt, obv).done(function(patient, obv) {
-          var byCodes = smart.byCodes(obv, 'code');
+        $.when(pt, condition).done(function(patient, conditions) {
           var gender = patient.gender;
           var dob = new Date(patient.birthDate);
           var day = dob.getDate();
@@ -46,11 +45,11 @@
             lname = patient.name[0].family.join(' ');
           }
 
-          var height = byCodes('8302-2');
-          var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
-          var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
-          var hdl = byCodes('2085-9');
-          var ldl = byCodes('2089-1');
+          // var height = byCodes('8302-2');
+          // var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
+          // var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
+          // var hdl = byCodes('2085-9');
+          // var ldl = byCodes('2089-1');
 
           var p = defaultPatient();
           p.birthdate = dobStr;
@@ -58,28 +57,22 @@
           p.fname = fname;
           p.lname = lname;
           p.age = parseInt(calculateAge(dob));
-          p.height = getQuantityValueAndUnit(height[0]);
+          // p.height = getQuantityValueAndUnit(height[0]);
 
-          if (typeof systolicbp != 'undefined')  {
-            p.systolicbp = systolicbp;
-          }
+          // if (typeof systolicbp != 'undefined')  {
+          //   p.systolicbp = systolicbp;
+          // }
+          //
+          // if (typeof diastolicbp != 'undefined') {
+          //   p.diastolicbp = diastolicbp;
+          // }
 
-          if (typeof diastolicbp != 'undefined') {
-            p.diastolicbp = diastolicbp;
-          }
+          // p.hdl = getQuantityValueAndUnit(hdl[0]);
+          // p.ldl = getQuantityValueAndUnit(ldl[0]);
 
-          p.hdl = getQuantityValueAndUnit(hdl[0]);
-          p.ldl = getQuantityValueAndUnit(ldl[0]);
+            p.conditions = conditions;
 
           ret.resolve(p);
-        });
-
-        $.when(pt, condition).done(function(patient, conditions) {
-          patient.conditions = conditions;
-
-
-          ret.resolve();
-          console.log('conditions', conditions);
         });
       } else {
         onError();
